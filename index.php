2b001fecd7f7964d54a1c27c8da9ca40af5b74dc
Nick Roewe <roewe@usc.edu>
2018-09-10 21:33:00 -0700

    connection_test.php
    we got sql server connection working!

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+	<div class="header">
+		<div>Homeowner Fraud</div>
+	</div>
+	<br>
+	<div class="login">
+		<form action="" method="post" enctype="multipart/form-data">
+			<div class="formDiv">
+				<input class="inputField" type="text" placeholder="Username" name="username"><br>
+				<input class="inputField" type="password" placeholder="Password" name="password"><br>
+			</div>
+			<div class="loginDiv">
+				<input type="submit" value="SignUp" name="login">
+			</div>
+		</form>
+		<div class="message"><?php //echo "Error: Incorrect Username / Password" ?></div>
+	</div>
+</body>
+</html>
