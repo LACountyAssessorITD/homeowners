b9e1e30fa94d097b802a1df39a99a83d2e850666
Henry Keane <hkeane@usc.edu>
2018-09-17 17:44:28 -0700

    InfoLookup Bug
    PHP Syntax :(

--- InfoLookup.php
+++ InfoLookup.php
@@ -16,12 +16,44 @@
 	
 	$homeownerSSN = intval($_GET['homeownerSSN']);;
 	//FOR TESTING PURPOSES: I WILL ONLY USE THE CLAIMANT, AND USE THE CLAIM TABLE FROM WRITE_CLAIM.PHP
-	$stmt = "SELECT claimantSSN FROM dbo.claim_table WHERE claimantSSN='$homeownerSSN'";
+	$sql = "SELECT claimantSSN FROM dbo.claim_table WHERE claimantSSN = '$homeownerSSN'";
 	/*
-	$stmt = "SELECT homeownerFirstname, homeownerLastname, homeownerSSN, spouseFirstname, spouseLastname,
+	$sql = "SELECT homeownerFirstname, homeownerLastname, homeownerSSN, spouseFirstname, spouseLastname,
 		spouseSSN, propertyAIN, propertyVacated, propertyAquired, propertyOccupied, propertyAddress, 
 		propertyApartment, propertyCity, propertyState, propertyZIP, claimNumber, taxYear FROM lookup_table";
 	*/
+	$stmt = sqlsrv_query( $conn, $sql );
+	
+	$sql = "SELECT FirstName, LastName FROM SomeTable";
+	
+	if( $stmt === false) {
+    	die( print_r( sqlsrv_errors(), true) );
+	}
+
+// 	while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+//       echo $row['LastName'].", ".$row['FirstName']."<br />";
+// 	}
+
+// 	sqlsrv_free_stmt( $stmt);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	if($stmt){
      	echo "Statement executed.<br>\n";
@@ -39,9 +71,11 @@
 	
 	while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))
 	{
-		echo "Col1: ".$row[claimantSSN]."\n";
+		echo "Col1: ".$row[0]."\n";
 		echo "-----------------<br>\n";
 	}
+	
+	
 	/* Free statement and connection resources. */
 	sqlsrv_free_stmt( $stmt);
 	sqlsrv_close( $conn);
